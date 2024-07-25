ac402b7213dae0197649e6df361f073955531f5f
Hardestylab4 <hardestylab4@dhcp164229.ctr.louisville.edu>
2024-07-25 16:01:01 -0400

    forgot to save

--- dummy.R
+++ dummy.R
@@ -37,3 +37,56 @@ rownames(x) <- rows
 colnames(x) <- cols
 
 sample_info <- data.frame(samples = colnames(x), groups = group_names)
+
+dummy_data <- function(group_min = 3, group_max = 20, dim_min = 1, dim_max = 50, param_min = 0, param_max = 20) {
+    group_num <- floor(runif(1, min = group_min, max = group_max))
+
+    group_names <- c()
+    for (i in 1:group_num) {
+        group_name <- paste0("group", i)
+        group_names <- append(group_names, group_name)
+    }
+
+    dims <- ceiling(runif(2, min = dim_min, max = dim_max))
+    col_num <- dims[1] * group_num
+    row_num <- dims[2]
+
+    parms <- ceiling(runif(2, min = param_min, max = param_max))
+    m <- parms[2]
+    v <- parms[1]
+
+    x <- data.frame()
+    cols <- c()
+    for (i in 1:col_num) {
+        n <- rnorm(row_num, m, v)
+        c <- paste0("Y", i)
+        cols <- append(cols, c)
+        x <- rbind(x, n)
+    }
+
+    x <- t(x)
+
+    rows <- c()
+    for (i in 1:row_num) {
+        r <- paste0("X", i)
+        rows <- append(rows, r)
+    }
+
+    rownames(x) <- rows
+    colnames(x) <- cols
+
+    sample_info <- data.frame(samples = colnames(x), groups = group_names)
+
+    dum <- c()
+    dum[[1]] <- x
+    dum[[2]] <- sample_info
+
+    return(dum)
+}
+
+d <- dummy_data()
+x <- d[[1]]
+sample_info <- d[[2]]
+
+
+
--- new.R
+++ new.R
@@ -1,3 +1,6 @@
+library(onewaytests)
+library(normtest)
+
 new_dat <- data.frame()
 cols <- c()
 for (i in 1:20) {
@@ -18,8 +21,6 @@ for (i in 1:1000) {
 
 rownames(new_dat) <- rows
 colnames(new_dat) <- cols
-
-library(onewaytests)
 
 sample_info <- data.frame(sample = colnames(new_dat), group = c(rep("Group1", 5), rep("Group2", 5), rep("Group3", 5), rep("Group4", 5)))
 
@@ -35,32 +36,33 @@ w$p.value == welch_res[1, ]
 
 library(PMCMRplus)
 
-new_dat <- data.frame()
+x <- data.frame()
 cols <- c()
 for (i in 1:20) {
     n <- rnorm(1000, 0, 1)
-    x <- paste("X", i, sep="")
-    y <- paste("Y", i, sep="")
-    cols <- append(cols, y)
-    new_dat <- rbind(new_dat, n)
+    c <- paste("Y", i, sep="")
+    cols <- append(cols, c)
+    x <- rbind(x, n)
 }
 
-new_dat <- t(new_dat)
+x <- t(x)
 
 rows <- c()
 for (i in 1:1000) {
-    x <- paste("X", i, sep="")
-    rows <- append(rows, x)
+    a <- paste("X", i, sep="")
+    rows <- append(rows, a)
 }
 
-rownames(new_dat) <- rows
-colnames(new_dat) <- cols
+rownames(x) <- rows
+colnames(x) <- cols
 
-sample_info <- data.frame(sample = colnames(new_dat), group = c(rep("Group1", 5), rep("Group2", 5), rep("Group3", 5), rep("Group4", 5)))
+sample_info <- data.frame(sample = colnames(x), group = c(rep("Group1", 5), rep("Group2", 5), rep("Group3", 5), rep("Group4", 5)))
 
-x <- run_dunnett(head(new_dat), sample_info$group)
-for (i in 1:nrow(head(new_dat))) {
-  d <- PMCMRplus::dunnettT3Test(unlist(new_dat[i, ]), g = as.factor(sample_info$group))
+z <- run_dunnett(head(x), sample_info$group)
+
+test <- NULL
+for (i in 1:nrow(head(x))) {
+  d <- PMCMRplus::dunnettT3Test(unlist(x[i, ]), g = as.factor(sample_info$group))
   p <- d$p.value
 
   pvals <- NULL
@@ -70,12 +72,173 @@ for (i in 1:nrow(head(new_dat))) {
   pvals <- pvals[, colSums(is.na(pvals)) == 0]
   pvals <- t(as.data.frame(pvals))
 
-  for (j in 1:ncol(pvals)) {
-    print(all.equal(pvals[[j]], x[i, j], tolerance = 0.0001))
-  }
+  test <- rbind(test, pvals)
 }
 
-for (j in 1:ncol(pvals)) {
-    print(pvals[[j]])
-    print(x[6, j])
+colnames(test) <- colnames(z)
+rownames(test) <- rownames(z)
+
+for (i in 1:nrow(test)) {
+    print(all.equal(test[i, ], z[i, ], tolerance = 0.0001))
 }
+
+for (i in 1:nrow(test)) {
+    for (j in 1:ncol(test)) {
+        print(all.equal(test[i, j], z[i, j], tolerance = 0.0001))
+    }
+}
+
+###
+library(normtest)
+library(PMCMRplus)
+library(testthat)
+
+d <- dummy_data(group_max = 6)
+x <- d[[1]]
+sample_info <- d[[2]]
+
+z <- run_dunnett(x, sample_info$groups)
+
+res <- NULL
+for (i in 1:nrow(x)) {
+  d <- PMCMRplus::dunnettT3Test(unlist(x[i, ]), g = as.factor(sample_info$group))
+  p <- d$p.value
+
+  pvals <- NULL
+  for (i in p) {
+      pvals <- cbind(pvals, i)
+  }
+  pvals <- pvals[, colSums(is.na(pvals)) == 0]
+  pvals <- t(as.data.frame(pvals))
+
+  res <- rbind(res, pvals)
+}
+
+colnames(res) <- colnames(z)
+rownames(res) <- rownames(z)
+
+for (i in 1:nrow(res)) {
+    for (j in 1:ncol(res)) {
+        print(all.equal(res[i, j], z[i, j], tolerance = 0.0002))
+    }
+}
+
+###############
+#### Tukey ####
+###############
+
+d <- dummy_data()
+x <- d[[1]]
+sample_info <- d[[2]]
+
+tukey_res <- run_tukey(x, sample_info$groups)
+
+mod <- aov(unlist(x[1, ]) ~ sample_info$groups)
+tt <- TukeyHSD(mod)
+res <- as.data.frame(tt[1])
+res <- t(res)
+res <- as.data.frame(res[4, ])
+res <- t(res)
+
+big_res <- NULL
+for (i in 1:nrow(x)) {
+    mod <- aov(unlist(x[i, ]) ~ sample_info$groups)
+    tt <- TukeyHSD(mod)
+    res <- as.data.frame(tt[1])
+    res <- t(res)
+    res <- as.data.frame(res[4, ])
+    res <- t(res)
+    big_res <- rbind(big_res, res)
+}
+
+for (i in 1:nrow(big_res)) {
+    for (j in 1:ncol(big_res)) {
+        print(all.equal(big_res[i, j], tukey_res[i, j]))
+    }
+}
+
+################
+##### Dunn #####
+################
+
+d <- dummy_data(group_max = 5)
+x <- d[[1]]
+sample_info <- d[[2]]
+
+dunn_res <- run_dunn(x, sample_info$groups)
+
+dunn_test <- NULL
+for (i in 1:nrow(x)) {
+    dunn <- Adunn.test(unlist(x[i, ]), g=sample_info$groups, method="bonferroni", table = FALSE, kw = FALSE, altp = TRUE)
+    dunn_padj <- as.data.frame(dunn[4])
+    dunn_padj <- t(dunn_padj)
+
+    dunn_test <- rbind(dunn_test, dunn_padj)
+}
+
+grp_list <- unique(sample_info$groups)
+grp_list <- grp_list[order(grp_list)]
+
+col_names <- c()
+counter <- 1
+for (g in grp_list) {
+    counter <- counter +  1
+    if (counter <= length(grp_list)) {
+        for (i in counter:length(grp_list)) {
+        the_name <- paste0(g, " - ", grp_list[i])
+        col_names <- append(col_names, the_name)
+        }
+    }
+}
+
+dunn_test <- dunn_test[, match(col_names, dunn$comparisons)]
+colnames(dunn_test) <- col_names
+
+for (i in 1:nrow(dunn_test)) {
+    for (j in 1:ncol(dunn_test)) {
+        print(all.equal(dunn_test[i, j], dunn_res[i, j]))
+    }
+}
+
+###########################
+### two_group_test_norm ###
+###########################
+library(normtest)
+library(car)
+
+d <- dummy_data()
+x <- d[[1]]
+sample_info <- d[[2]]
+
+res <- two_group_test_norm(x, sample_info$groups)
+
+pass_shap <- NULL
+for (i in 1:nrow(x)) {
+    rows <- rownames(x)
+    mod <- glm(unlist(x[i, ]) ~ sample_info$groups)
+    resids <- mod$residuals
+    shap <- shapiro.test(resids)
+    if (shap$p.value >= 0.05) {
+        pass_shap <- rbind(pass_shap, rows[i])
+    }
+}
+pass1 <- x[pass_shap, ]
+
+pass_lev <- NULL
+for (i in 1:nrow(pass1)) {
+    rows <- rownames(pass1)
+    mod <- glm(unlist(pass1[i, ]) ~ sample_info$groups)
+    resids <- mod$residuals
+    vari <- leveneTest(resids ~ sample_info$groups)
+    if (vari$Pr[1] >= 0.05) {
+        pass_lev <- rbind(pass_lev, rows[i])
+    }
+}
+pass2 <- x[pass_lev, ]
+#pass1 passed shapiro
+#pass2 passed both shapiro and levene
+all.equal(pass2, res[[1]])
+#setdiff(pass1, pass2) passed shapiro and not levene
+all.equal(setdiff(pass1, pass2), res[[2]], check.attributes = FALSE)
+#setdiff(x, union(pass1, pass2)) passed neither shapiro nor levene
+all.equal(setdiff(x, union(pass1, pass2)), res[[3]], check.attributes = FALSE)
